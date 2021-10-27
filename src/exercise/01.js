bc9e2a2e24956cbb7dd99eb3294ec0b0dcad9d36
Nicole Furlan <nfurlan@alley.co>
2021-10-27 12:31:26 -0400

    Exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,8 +4,6 @@
 import * as React from 'react'
 import {dequal} from 'dequal'
 
-// ./context/user-context.js
-
 import * as userClient from '../user-client'
 import {useAuth} from '../auth-context'
 
@@ -73,14 +71,14 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 
-// export {UserProvider, useUser}
-
-// src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -97,12 +95,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState);
   }
 
   return (
