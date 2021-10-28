5af989740cec104bfdb185e25e7dc8383afe8729
Nicole Furlan <nfurlan@alley.co>
2021-10-28 13:16:12 -0400

    Exercise 2 extra credit 1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -9,10 +9,12 @@ function Toggle({children}) {
   const toggle = () => setOn(!on)
 
   return React.Children.map(children, child => {
-    return React.cloneElement(child, {
-      on,
-      toggle,
-    })
+    return (typeof child.type === 'string')
+      ? child
+      : React.cloneElement(child, {
+        on,
+        toggle,
+      })
   });
 }
 
@@ -26,6 +28,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
